74c0bf97df83e23035626330cf0ad3865536d54d
ooowzyooo <144597515+ooowzyooo@users.noreply.github.com>
2024-10-01 23:38:41 +0800

    Add files via upload

---
+++ crowdfunding_db(1).sql
@@ -0,0 +1,83 @@
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: localhost    Database: crowdfunding_db
+-- ------------------------------------------------------
+-- Server version	8.0.39
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `CATEGORY_ID` int NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`CATEGORY_ID`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Disaster Relief'),(2,'Arts & Culture'),(3,'Youth Empowerment');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fundraiser`
+--
+
+DROP TABLE IF EXISTS `fundraiser`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fundraiser` (
+  `FUNDRAISER_ID` int NOT NULL AUTO_INCREMENT,
+  `ORGANIZER` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `CAPTION` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `TARGET_FUNDING` decimal(10,2) NOT NULL,
+  `CURRENT_FUNDING` decimal(10,2) DEFAULT '0.00',
+  `CITY` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `ACTIVE` tinyint(1) NOT NULL DEFAULT '1',
+  `CATEGORY_ID` int DEFAULT NULL,
+  PRIMARY KEY (`FUNDRAISER_ID`) USING BTREE,
+  KEY `CATEGORY_ID` (`CATEGORY_ID`) USING BTREE,
+  CONSTRAINT `fundraiser_ibfk_1` FOREIGN KEY (`CATEGORY_ID`) REFERENCES `category` (`CATEGORY_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fundraiser`
+--
+
+LOCK TABLES `fundraiser` WRITE;
+/*!40000 ALTER TABLE `fundraiser` DISABLE KEYS */;
+INSERT INTO `fundraiser` VALUES (1,'Rachel Adams','South Gaza: Tents, Cash Aid, & Medical',20000.00,8000.00,'Houston',1,1),(2,'Samuel Brooks','North and Central Gaza: Food, Water, Diapers + Milk, Medical Aid',5000.00,2500.00,'New York',1,2),(3,'Olivia Cooper','Pardon Donziger',12000.00,4000.00,'Los Angeles',0,3),(4,'Ethan Davis','These Guardians need an angel',15000.00,6000.00,'Miami',1,1),(5,'Sophia Evans','The Last Fund Raiser for Life of Pikelet Rescue',7000.00,3000.00,'Chicago',1,2);
+/*!40000 ALTER TABLE `fundraiser` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-01 23:18:23
